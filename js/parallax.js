a843c7e7641bd64da82a283671508ac5d024c319
WilsonVo <wilson_vo@brown.edu>
2024-08-23 18:38:42 -0400

    Feat: added font api, fixed parallax

--- js/parallax.js
+++ js/parallax.js
@@ -4,3 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 scale: 1.5
             });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    var images = document.querySelectorAll('.banner-image2');
+            new simpleParallax(images, {
+                scale: 1.3
+            });
+});
